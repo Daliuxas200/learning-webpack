57b68d0b5c5e2df93b5b4d8421bd88aa7b500366
Dalius Slavickas <slavickasd@gmail.com>
2020-09-03 16:00:41 +0300

    final version of this tutorial section

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './pepe.jpg';
-import Data from './data.csv';
+import printMe from './print.js';
 
 function component() {
-  const element = document.createElement('div');
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-  console.log(Data);
+    element.appendChild(btn);
 
-  return element;
+    return element;
 }
 
 document.body.appendChild(component());
